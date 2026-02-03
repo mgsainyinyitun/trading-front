50d16a833e187c6007f623f59b2efb41cc15b8a1
Sai Nyi Nyi Tun <mgsainyinyitun.tumdy@gmail.com>
2026-02-03 18:04:02 +0900

    change empty box

--- src/components/common/EmptyBox.jsx
+++ src/components/common/EmptyBox.jsx
@@ -1,9 +1,9 @@
 import { Box } from "@mui/material";
 import empty from '../../images/general/empty.png';
-export default function EmptyBox(){
-    return(
+export default function EmptyBox() {
+    return (
         <Box width='100%' height='100%' display='flex' justifyContent='center' alignItems='center'>
-            <img src={empty} width={100}/>
+            <img src={empty} width={100} alt="empty" />
         </Box>
     );
 }
